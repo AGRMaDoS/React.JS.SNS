e07c4179c5b6663fcadf96841bd078c10e5ed254
mados <nlxx@mail.ru>
2024-09-12 20:36:50 +0300

    51 урок

--- src/components/Find Users/FindUsers.jsx
+++ src/components/Find Users/FindUsers.jsx
@@ -1,62 +1,48 @@
 import React from "react";
 import style from "./FindUsers.module.css";
+import axios from "axios";
+import userPhoto from "../../assets/images/user.png";
 
 const findUsersPage = (props) => {
-    if(props.users.length===0){
-        props.setUsers([
-            {
-                id: 1,
-                photoUrl:'https://img.championat.com/s/732x488/news/big/r/b/10-priznakov-nastoyaschego-muzhchiny-prover-sebya_1671197209553984706.jpg',
-                followed: false,
-                fullName: 'Dmitry',
-                status: 'I am a boss',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 2,
-                photoUrl:'https://img.championat.com/s/732x488/news/big/r/b/10-priznakov-nastoyaschego-muzhchiny-prover-sebya_1671197209553984706.jpg',
-                followed: true,
-                fullName: 'Sasha',
-                status: 'I am a boss too',
-                location: {city: 'Moscow', country: 'Russia'}
-            },
-            {
-                id: 3,
-                photoUrl:'https://img.championat.com/s/732x488/news/big/r/b/10-priznakov-nastoyaschego-muzhchiny-prover-sebya_1671197209553984706.jpg',
-                followed: false,
-                fullName: 'Dmitry',
-                status: 'I am a boss too',
-                location: {city: 'Kiev', country: 'Ukraine'}
-            },
-        ])
+    if (props.users.length === 0) {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+            .then((response) => {
+                debugger;
+                props.setUsers(response.data.items)
+            })
     }
 
-   return <div>
-       {
-           props.users.map(u=> <div key={u.id}>
+    return <div>
+        {
+            props.users.map(u => <div key={u.id}>
             <span>
                 <div>
-                    <img src={u.photoUrl} className={style.avatar} alt='Avatar'/>
+                    <img src={u.photos.small != null ? u.photos.small : userPhoto} className={style.avatar}
+                         alt='Avatar'/>
                 </div>
                 <div>
-                    { u.followed
-                        ? <button onClick={()=>{props.unfollow(u.id)}}>Unfollow</button>
-                        :<button onClick={()=>{props.follow(u.id)}}>Follow</button>}
+                    {u.followed
+                        ? <button onClick={() => {
+                            props.unfollow(u.id)
+                        }}>Unfollow</button>
+                        : <button onClick={() => {
+                            props.follow(u.id)
+                        }}>Follow</button>}
                 </div>
             </span>
-               <span>
+                <span>
                    <span>
-                       <div>{u.fullName}</div>
+                       <div>{u.name}</div>
                        <div>{u.status}</div>
                    </span>
                    <span>
-                       <div>{u.location.country}</div>
-                       <div>{u.location.city}</div>
+                       <div>{"u.location.country"}</div>
+                       <div>{"u.location.city"}</div>
                    </span>
                </span>
-           </div>)
-       }
-   </div>
+            </div>)
+        }
+    </div>
 
 }
 
